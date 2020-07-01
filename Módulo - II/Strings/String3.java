72adc4713b921cdf335428443ddb07a62efc9a8f
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-07-01 20:22:16 -0300

    Trabalhando com String

--- Módulo - II/Strings/String3.java
+++ Módulo - II/Strings/String3.java
@@ -1,26 +1,32 @@
 import java.io.IOException;
 import java.util.Scanner;
- 
+
 public class String3 {
-// através da claúsula throws indicamos que não iremos
-// tratar possíveis erros na entrada de dados realizada
-// através do método "read" do pacote de classes System.in
+  // através da claúsula throws indicamos que não iremos
+  // tratar possíveis erros na entrada de dados realizada
+  // através do método "read" do pacote de classes System.in
   public static void main(String[] args) throws IOException {
     Scanner ler = new Scanner(System.in);
- 
+
     String nome;
     char sexo;
- 
+
     System.out.printf("Informe um nome:\n");
     nome = ler.nextLine();
- 
+
     System.out.printf("\nInforme o sexo (M/F):\n");
-    //recebendo um caracter
-    sexo = (char)System.in.read();
- 
+    // recebendo um caracter
+    sexo = (char) System.in.read();
+
     if ((sexo == 'M') || (sexo == 'm'))
-       System.out.printf("\nSeja bem-vindo Sr. %s.\n", nome);
-    else System.out.printf("\nSeja bem-vinda Sra. %s.\n", nome);
+      System.out.printf("\nSeja bem-vindo Sr. %s.\n", nome);
+    else
+      System.out.printf("\nSeja bem-vinda Sra. %s.\n", nome);
+
+    /*
+     * if ((s.charAt(i) >= 'A') && (s.charAt(i) <= 'Z')) ctmaius = ctmaius + 1;
+     */
+
   }
- 
+
 }
