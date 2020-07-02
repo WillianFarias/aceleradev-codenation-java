cee1853c6711aa984690d28f55bbb1423aec35a2
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-07-02 17:14:44 -0300

    DDD - Java exemplo

---
+++ Módulo - II/dddCod/Aviao.java
@@ -0,0 +1,9 @@
+public class Aviao {
+
+  private Long id;
+  private String nome;
+
+  private Fabricante fabricante;
+  private Cor cor;
+  private Capacidade capacidade;
+}
---
+++ Módulo - II/dddCod/Capacidade.java
@@ -0,0 +1,22 @@
+public class Capacidade {
+
+  private String potencia;
+  private String quantidadePassageiro;
+
+  public String getPotencia() {
+    return potencia;
+  }
+
+  public void setPotencia(String potencia) {
+    this.potencia = potencia;
+  }
+
+  public String getQuantidadePassageiro() {
+    return quantidadePassageiro;
+  }
+
+  public void setQuantidadePassageiro(String quantidadePassageiro) {
+    this.quantidadePassageiro = quantidadePassageiro;
+  }
+
+}
