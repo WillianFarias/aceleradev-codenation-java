370c579bca6fd09c5024283eb26d7d11bf496c61
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-07-21 17:58:02 -0300

    Config projeto artigo spring data jpa

---
+++ Módulo - IV/cod-artigo-spring-data-jpa-algaworks/artigo-spring-data-jpa-algaworks/src/test/java/com/algaworks/estoque/ArtigoSpringDataJpaAlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtigoSpringDataJpaAlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
