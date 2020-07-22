a5963c43c615d6c2d0e32c00319dfb939ab55858
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-07-22 13:07:04 -0300

    Finalizacao cod artigo spring data jpa

--- artigo-spring-data-jpa/src/main/java/com/algaworks/estoque/repository/Produtos.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.estoque.repository;
-
-import com.algaworks.estoque.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Produtos extends JpaRepository<Produto, Long> {
-}
