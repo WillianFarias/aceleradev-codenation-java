4298ae0ed0a8d2192da48b5231dd4817d8b04916
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-08-10 12:06:06 -0300

    Application employees RESTful

---
+++ Módulo - VI/tutorial-rest/src/main/java/br/com/codenation/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package br.com.codenation.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	EmployeeNotFoundException(Long id) {
+		super("Could not find employee " + id);
+	}
+}
---
+++ Módulo - VI/tutorial-rest/src/main/java/br/com/codenation/payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package br.com.codenation.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
