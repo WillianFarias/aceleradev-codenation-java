773f00c42d2539a16dd80735b9b3210720064139
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-08-13 08:21:43 -0300

    Incersao de dependencia mapstruct e criacao dos entitys

---
+++ Módulo - VI/MapStruct/tuto-map-struct/src/main/java/br/com/codenation/entity/SimpleSource.java
@@ -0,0 +1,23 @@
+package br.com.codenation.entity;
+
+public class SimpleSource {
+
+  private String name;
+  private String description;
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setNome(String name){
+    this.name = name;
+  }
+
+  public String getDescription(){
+    return this.description;
+  }
+
+  public void setDescription(String description){
+    this.description = description;
+  }
+}
