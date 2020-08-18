382a64670ef5198a59ec3be1e6b92f6cc4ea153e
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-08-18 14:39:41 -0300

    SegurancaConfig e application.properties

---
+++ Módulo - VII/tuto-spring-security/src/main/java/br/com/codenation/oauth2/tutospringsecurity/config/SegurancaConfig.java
@@ -0,0 +1,19 @@
+package br.com.codenation.oauth2.tutospringsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+//Dependencias necessarias para o uso do oauth
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SegurancaConfig {
+
+}
